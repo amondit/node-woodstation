552764c2c23f6032a8e2a7b8e5034b81977ebd68
Arnaud Mondit <a.mondit@gmail.com>
2015-06-02 11:08:38 -0400

    Update woodstation.js

--- woodstation.js
+++ woodstation.js
@@ -4,11 +4,14 @@ var moment = require('moment');
 
 
 var display = HT1632.initialize("/dev/spidev0.0", HT1632.mode.8PMOS);
+display.clear();
+display.pwm(15);
+display.blink(false);
 
 var nowString = moment().format("HHmmssDDMMYYYY");
 
 // 7 segment digit display
-var 7segmentDigits	= [[true, true , true, true, true, true, false], \
+var 7segmentDigits = [[true, true , true, true, true, true, false], \
 		[false, true , true, false, false, false, false], \
 		[true, true , false, true, true, false, true], \
 		[true, true , true, true, false, false, true], \
@@ -19,15 +22,44 @@ var 7segmentDigits	= [[true, true , true, true, true, true, false], \
 		[true, true , true, true, true, true, true], \
 		[true, true , true, false, false, true, true]];
 
+//Time / date digits addresses on the Woodstation display (HHmmssDDMMYYYY format)
+var dateTimeDisplayAddresses = [[0,2], \
+		[14,2], \
+		[28,2], \
+		[0,3], \
+		[14,3], \
+		[28,3], \
+		[1,0], \
+		[15,0], \
+		[29,0], \
+		[1,1], \
+		[15,1], \
+		[29,1], \
+		[1,2], \
+		[15,2]];
+
 setInterval(function(){
         var nowNewString = moment().format("HHmmssDDMMYYYY");
         if (nowNewString !== nowString) {
         	nowString = nowNewString;
-        	updateDisplay();
+        	updateDateTimeDisplay();
         };
 //      console.log(nowString);
 },100);
 
-function updateDisplay(){
-
+function updateDateTimeDisplay(){
+	//switch on display indicators (':' in time, 'D' and 'M' on date)
+	display.writeLed(41,3,true);
+	display.writeLed(1,3,true);
+	display.writeLed(11,3,true);
+	display.writeLed(15,3,true);
+	for (var i = 0; i < nowString.length; i++) {
+		var digit = parseInt(nowString[i]);
+		var writeAddress = dateTimeDisplayAddresses[i][0];
+		var writeLed = dateTimeDisplayAddresses[i][1];
+		for (var j = 0; j < 7segmentDigits[digit].length; j++) {
+			display.writeLed(writeAddress, writeLed, 7segmentDigits[digit][j]);
+			writeAddress = writeAddress + 2;
+		};
+	};
 };
